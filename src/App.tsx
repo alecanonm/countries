7978aab4a51eb57a1b04c7ff5e9f47dc1149e7ae
Alejandro Canon <al3jandrocan0n@gmail.com>
2024-02-28 13:18:11 -0500

    Feature: Card component

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import { SearchBar } from './components/SearchBar'
 function App() {
   return (
     <div className='grid grid-rows-2 gap-10'>
-      <section className='row-span-2  flex flex-col items-center gap-10 sm:flex-row justify-between px-8 sm:px-24'>
+      <section className='row-span-2 flex flex-col sm:items-center  gap-10 sm:flex-row justify-between px-8 sm:px-24'>
         <SearchBar />
         <DropMenu />
       </section>
-      <section className=' flex items-center justify-center px-8'>
+      <section className='flex items-center justify-center'>
         <Card />
       </section>
     </div>
