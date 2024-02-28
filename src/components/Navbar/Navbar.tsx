f34f5c0f1896841c4f5ed661af9c793b54ff5f28
Alejandro Canon <al3jandrocan0n@gmail.com>
2024-02-28 10:27:01 -0500

    Feature: tools section

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,10 @@
 export default function Navbar() {
   return (
     <nav className='flex justify-between p-6 items-center bg-lightModeBackground shadow-sm sm:px-20'>
-      <h1 className='sm:text-2xl font-[760]'>Where in the world?</h1>
+      <h1 className='sm:text-2xl font-extrabold'>Where in the world?</h1>
       <button className='flex gap-2'>
         <img src='moon.png' alt='light moon' width={20} height={18} />
-        <span className='font-[700]'>Dark Mode</span>
+        <span className='font-bold'>Dark Mode</span>
       </button>
     </nav>
   )
